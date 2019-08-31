08acf56789fe3cd2a72ae502149d50b92eafd395
Vladimir Gumennikov <vova@vova.info>
2019-08-31 18:31:09 +1000

    1st commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const numbers = [2, 3, 5];
+const doubledNumbers = numbers.map(number => number * 2);
+import "./style.css";
+
+console.log(doubledNumbers); // 4, 6, 10
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+
+module.exports = {
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css'
+        }),
+        new HtmlWebpackPlugin({
+            // Означает, что:
+            inject: false, // стили НЕ нужно прописывать внутри тегов
+            hash: true, // для страницы нужно считать хеш
+            template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
+            filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
+        })
+    ]
+};
